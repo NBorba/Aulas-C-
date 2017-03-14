4cee3be19e4d625d1a353b91c5eb941805a2cb1a
NBorba <renanblucas@gmail.com>
2017-03-14 19:35:59 -0300

    Calculadora funcionando sem validacao de valor

--- Calculadora100317/Calculadora100317/Util.cs
+++ Calculadora100317/Calculadora100317/Util.cs
@@ -9,32 +9,6 @@ namespace Calculadora100317
 {
     class Util
     {
-        public int calculateValues(string operation, int numberOne, int numberTwo)
-        {
-            int resultado = 0;
-
-            if (operation == "+")
-            {
-                resultado = numberOne + numberTwo;
-            }
-
-            if (operation == "-")
-            {
-                resultado = numberOne - numberTwo;
-            }
-
-            if (operation == "*")
-            {
-                resultado = numberOne * numberTwo;
-            }
-
-            if (operation == "/")
-            {
-                resultado = numberOne / numberTwo;
-            }
-            return resultado;
-        }
-
         public bool verifyText(string output)
         {
             if (output.Length == 0)
@@ -50,70 +24,31 @@ namespace Calculadora100317
             return true;
         }
 
-        private int arrayCount = 0;
-        private int arrayPositionCount = 1;
-        private int tamanhoTextoAnterior = 0;
-
-        public int CalculateFormula(string output, string operationText)
+        public int calculateFormula(string operation, int numberOne, int numberTwo)
         {
+            int result = 0;
 
-            string[] operations = new string[4] { "+", "-", "/", "*" };
-            int[] number = new int[2];
-            string operation = "";
-
-            // Verifies if formula starts with an operation, if it does, formula is invalid
-            bool canAddOperation = verifyText(output);
-
-            int textLength = output.Length;
-            int resultado = 0;
-            string numero = "";
-
-            if (!canAddOperation)
+            if (operation == "+")
             {
-                MessageBox.Show("Erro de formula.", "Erro",
-                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                result = numberOne + numberTwo;
             }
-            else
+
+            if (operation == "-")
             {
-
-                if (arrayPositionCount == 1)
-                {
-                    operation = operationText;
-                }
-
-                string[] numbers = output.Split(new string[] { "+", "-", "*", "/" }, StringSplitOptions.None);
-
-                numero = numbers[arrayCount];
-
-                number[arrayCount] = (Convert.ToInt32(numero));
-
-                tamanhoTextoAnterior += numero.Length;
-
-                if (arrayCount >= 1)
-                {
-                    resultado = calculateValues(operation, number[arrayCount - 1], number[arrayCount]);
-                    number[0] = number[arrayCount - 1];
-                    number[1] = 0;
-
-                    clrData();
-                    tamanhoTextoAnterior = resultado.ToString().Length;
-                    output += resultado;
-                }
-
-                if (resultado == 0)
-                {
-                    output += operation;
-                    arrayCount++;
-                    arrayPositionCount++;
-                }
-                return resultado;
+                result = numberOne - numberTwo;
             }
-        }
 
-        private void clrData()
-        {
-            arrayCount = 0;
-            arrayPositionCount = 1;
+            if (operation == "*")
+            {
+                result = numberOne * numberTwo;
+            }
+
+            if (operation == "/")
+            {
+                result = numberOne / numberTwo;
+            }
+            return result;
         }
+     
     }
 }
--- Calculadora100317/Calculadora100317/Views/CalcForm.cs
+++ Calculadora100317/Calculadora100317/Views/CalcForm.cs
@@ -8,9 +8,6 @@ namespace Calculadora100317
 {
     public partial class formCalc : Form
     {
-       
-     
-
         Util util = new Util();
 
         public formCalc()
@@ -21,8 +18,7 @@ namespace Calculadora100317
         private void btnNumberClick(object sender, EventArgs e)
         {
             var btnClickedNumber = sender as Button;
-
-                txtOutput.Text += btnClickedNumber.Text;
+            txtOutput.Text += btnClickedNumber.Text;
         }
 
         private void btnClearOutput(object sender, EventArgs e)
@@ -33,11 +29,45 @@ namespace Calculadora100317
         private void btnOperationClick(object sender, EventArgs e)
         {
             var btnClickedOperation = sender as Button;
+            
+            string formula = txtOutput.Text;
 
-            string text = txtOutput.Text;
+            // Verifies if formula starts with an operation, if it does, formula is invalid
+            bool canAddOperation = util.verifyText(formula);
 
-            int resultado = util.CalculateFormula(text);
+            if(canAddOperation)
+            { 
+                txtOutput.Text += btnClickedOperation.Text;
+                formula = txtOutput.Text;
 
+                string[] operators = formula.Split(new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" },StringSplitOptions.RemoveEmptyEntries);
+                string[] numbers = formula.Split(new string[] { "+", "-", "*", "/", "="}, StringSplitOptions.RemoveEmptyEntries);
+
+                int numbersCount = numbers.Count();
+                int operatorsCount = operators.Count();
+
+                if(numbersCount == 2 && operatorsCount == 2)
+                {
+                    int numberOne = Convert.ToInt32(numbers[0]);
+                    int numberTwo = Convert.ToInt32(numbers[1]);
+
+                    string operatorOne = operators[0];
+                    string operatorTwo = operators[1];
+
+                    int result = util.calculateFormula(operatorOne, numberOne, numberTwo);
+
+                    if (btnClickedOperation.Text == "=")
+                    {
+                        clearOutput();
+                        txtOutput.Text = result.ToString();
+                    }
+                    else
+                    {
+                        clearOutput();
+                        txtOutput.Text = result + operatorTwo;
+                    }
+                }
+            }
         }
 
         private void btnEqualsClick(object sender, EventArgs e)
